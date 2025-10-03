871e6b7233f1ce603eeaad89df380aa592d59095
MF-ROB <maximiliano.fairman@stu.ocsb.ca>
2025-10-03 10:08:49 -0400

    Add files via upload

---
+++ hello_world_style.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Created By: Your Name
+# Date: Month Day, Year
+# Write what this program does.
+def main():
+    print("hello world I am cool")
+    print("I am doing cool coding like hacker")
+
+
+if __name__ == "__main__":
+    main()
